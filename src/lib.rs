b4b53a97a9ce9374a19d2d907f6075a8b2c5a893
M Farkas-Dyck <strake888@gmail.com>
2018-07-21 11:11:35 -0800

    allow(unused_unsafe)

--- src/lib.rs
+++ src/lib.rs
@@ -231,7 +231,7 @@ fn ptr_diff<A>(p: *const A, q: *const A) -> usize {
 
 #[macro_export]
 macro_rules! str0 {
-    ($s:expr) => (unsafe {
+    ($s:expr) => (#[allow(unused_unsafe)] unsafe {
         $crate::Nul::<u8>::new_unchecked_mut(concat!($s, "\0").as_ptr() as *mut _)
     })
 }
