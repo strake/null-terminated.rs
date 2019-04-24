d01afa7aeab643d83479c768eac9c5ea4d4874d9
M Farkas-Dyck <strake888@gmail.com>
2019-04-24 14:44:52 -0800

    doc crate a little further

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! Library of [null-terminated slices](struct.Nul.html) and
 //! [UTF-8-encoded strings](struct.NulStr.html), references to which are thin pointers for
-//! efficiency and ease of use with FFI.
+//! efficiency and ease of use with FFI
 //!
-//! A likely common use case is C FFI, for example:
+//! The likely primary use cases are C FFI and OS ABI (for example: on Unix, many system calls
+//! take, and the initial environment involves, null-terminated arguments).
+//!
+//! As the representation is a bare pointer to the element type, one can declare foreign
+//! functions which merely take arguments or return values of type `Nul<_>`, for example:
 //! ```no_run
 //! # extern crate null_terminated; use null_terminated::Nul; type c_int = i32;
 //! extern "C" {
